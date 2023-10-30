4b86e639316ed28f4412c7ab133b36b26f69ea58
rafapcmor <rafapcmor@gmail.com>
2023-10-30 17:24:29 -0300

    Exemplos de assercoes

---
+++ src/main/java/Dog.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.assertionsjunit;
+
+/**
+ *
+ * @author rafaelamoreira
+ */
+public class Dog {
+
+    private String nome;
+
+    public Dog() {
+    }
+
+    public Dog(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
